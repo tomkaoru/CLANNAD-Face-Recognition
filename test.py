0fbff60d63fbc16992926cf00d31aba281467587
Ryota Torii <52717342+rtorii@users.noreply.github.com>
2022-06-25 17:52:47 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -8,16 +8,15 @@ characters = ["Fuko","Kotomi","Kyou","Nagisa","Ryou","Youhei","Tomoya","Tomoyo"]
 
 colors = [
 (0,128,0), #green
-(216,191,216),#thistle
+(200,170,200),#thistle
 (128,0,128),#purple
-(255,248,220),#cornsilk
-(255,0,255), #fuchsia
+(200,100,240),#cornsilk
+(180,0,180), #fuchsia
 (255,255,0),#yellow
 (0,0,128), #navy
-(192,192,192)# silver
+(200,200,200)# silver
 ]
 
-# @st.cache(allow_output_mutation=True)
 def load_nnmodel():
     model = load_model("model_200_160_80_epoch30_30.h5")
     return model
@@ -25,9 +24,14 @@ def load_nnmodel():
 def main():
     model = load_nnmodel()
     anime_cascade = cv2.CascadeClassifier('lbpcascade_animeface.xml') #save lbpcascade_animeface.xml in the samme directory as test.py
-    input_img = cv2.imread('test_pics/clannad-1.jpeg',1) #  test image
+    input_img = cv2.imread('test_pics/t02200154_0342024012866838688.jpeg',1) #  test image
     img_gray=cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
-    faces=anime_cascade.detectMultiScale(img_gray, scaleFactor=1.1, minNeighbors=2, minSize=(10,10))#
+
+    blank_img = np.zeros(input_img.shape,dtype=np.uint8)
+    blank_img.fill(255)
+    # cv2.imshow("face",blank_img)
+    # cv2.waitKey(0)
+    faces=anime_cascade.detectMultiScale(img_gray, scaleFactor=1.1, minNeighbors=5, minSize=(10,10))#
     if len(faces)>0:
         for (x,y,w,h) in faces:
             face = input_img[y:y+h, x:x+w]
@@ -38,10 +42,11 @@ def main():
             face_rgb = face_rgb.reshape(1, face_rgb.shape[0] * face_rgb.shape[1] * face_rgb.shape[2]).astype("float32")[0]
             result = np.argmax(model.predict(np.array([face_rgb / 255])), axis=-1)
             # print(characters[result[0]])
-            cv2.rectangle(input_img, pt1=(x, y), pt2=(x+w, y+h), color=colors[result[0]], thickness=3, lineType=cv2.LINE_8, shift=0)
-            cv2.putText(input_img, text=characters[result[0]], org=(x+1, y-1), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=colors[result[0]], thickness=3, lineType=cv2.LINE_4)
-    cv2.imshow("face",input_img)
+            cv2.rectangle(blank_img, pt1=(x, y), pt2=(x+w, y+h), color=colors[result[0]], thickness=3, lineType=cv2.LINE_8, shift=0)
+            cv2.putText(blank_img, text=characters[result[0]], org=(x+1, y-1), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=colors[result[0]], thickness=3, lineType=cv2.LINE_4)
+    cv2.imshow("face",blank_img)
     cv2.waitKey(0)
+    cv2.imwrite("output.jpg",blank_img)
 
 
 
